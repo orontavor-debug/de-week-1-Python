f1d9b7bb8fda3fc10d7c6b5d75b396af4f591c47
“orontavor-debug” <oron.tavor@gmail.com>
2026-03-17 16:29:09 +0100

    Add practice notebooks and solutions

---
+++ 05_pandas_intro/hello.py
@@ -0,0 +1,8 @@
+def safe_title(s):
+    splitted_list=[split.s]
+    joint_list=[]
+    for s in splitted_list:
+        if s.isupper:
+            joint_list=append.s
+        else:
+            joint_list=append.s
